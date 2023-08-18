dfc1d5ff250fff5e7d726624826f132b8cfeda39
AJ Scrivner <aj.hortobagyi@gmail.com>
2023-08-18 14:52:31 +0100

    Remove screws with screwdriver. London is trigger point on globe, closet opens.

---
+++ Assets/Scripts/globeCollisionDetection.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.XR;
+
+public class globeCollisionDetection : MonoBehaviour 
+{
+    //[SerializeField] private GameObject blackscreen;
+    [SerializeField] private GameObject dot;
+    [SerializeField] private Animator anim;
+
+
+    //[SerializeField] public GameObject ball;
+
+    //private int numberOfBalls;
+
+    //private bool playerEntered;
+
+
+    void Start()
+	{
+        //numberOfBalls = 0;
+        anim.SetBool("closetTriggered", false);
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+	{		
+		if (other.gameObject.CompareTag("dotGlobe"))		//player has collided with trigger
+		{
+            dot.gameObject.SetActive(true);
+            anim.SetBool("closetTriggered", true);
+
+
+        }
+    }
+/*
+	void OnTriggerExit(Collider other)
+	{
+        if (other.gameObject.CompareTag("Player"))       //player has collided with trigger
+        {
+            anim.SetBool("handEntered", false);
+
+        }
+    }*/
+
+
+
+}
---
+++ Assets/Scripts/screwDriverCollider.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.XR;
+
+public class screwDriverCollider : MonoBehaviour 
+{
+    [SerializeField] private GameObject paintingObject;
+    [SerializeField] private GameObject panelObject;
+
+    //[SerializeField] private GameObject winnerscreen;
+
+
+    //[SerializeField] public GameObject ball;
+
+    private int pictureScrews;
+    private int panelScrews;
+    private Rigidbody rb1;
+    private Rigidbody rb2;
+
+
+
+    //private bool playerEntered;
+
+
+    void Start()
+	{
+        pictureScrews = 0;
+        panelScrews = 0;
+
+        rb2 = panelObject.GetComponent<Rigidbody>();
+        rb1 = paintingObject.GetComponent<Rigidbody>();
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+	{		
+		if (other.gameObject.CompareTag("picScrew"))		//player has collided with trigger
+		{
+            other.gameObject.SetActive(false);
+            //GameObject.Find("ButtonCont").SetActive(false);
+            if (pictureScrews == 1)
+            {
+                rb1.isKinematic = false;
+                //paintingObject.SetActive(true);
+            }
+            else
+            {
+                pictureScrews++;
+            }
+            
+        }
+        else if (other.gameObject.CompareTag("panelScrew"))       //player has collided with trigger
+        {
+            other.gameObject.SetActive(false);
+            //GameObject.Find("ButtonCont").SetActive(false);
+            if (panelScrews == 3)
+            {
+                rb2.isKinematic = false;
+                //paintingObject.SetActive(true);
+            }
+            else
+            {
+                panelScrews++;
+            }
+
+        }
+    }
+/*
+	void OnTriggerExit(Collider other)
+	{
+        if (other.gameObject.CompareTag("Player"))       //player has collided with trigger
+        {
+            anim.SetBool("handEntered", false);
+
+        }
+    }*/
+
+
+
+}
