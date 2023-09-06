1ef5ed139137bb3e221bd20f188fe8c7f350450b
AJ Scrivner <aj.hortobagyi@gmail.com>
2023-09-06 15:13:30 +0100

    Countdown clock created and tested

---
+++ Assets/Scripts/countDownScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class countDownScript : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public TextMeshProUGUI TimerTxt;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(TimerOn)
+        {
+            if(TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTime(TimeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is UP!");
+                TimeLeft = 0;
+                TimerOn = false;
+            }
+        }
+    }
+
+    void updateTime(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+}
